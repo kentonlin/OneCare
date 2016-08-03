f05400732c0f799c45b41f276ce24dd88e5e3b8a
Kenton <kentonlin11@gmail.com>
2016-08-03 10:13:28 -0400

    sdlfkj

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var dbHelpers = require('../db/dbhelper.js');
-var path = require('path')
+var path = require('path');
 var app = express();
+
+app.use(express.static('public'));
 
 var rootPath = path.normalize(__dirname + '/../client');
 // app.use(express.static(__dirname + "/client"));
@@ -23,7 +25,7 @@ app.listen(process.env.PORT || 3000, function(){
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(rootPath + "/index.html"));
-})
+});
 
 app.get('/fuckDan', function(req, res){
   console.log('request received at /fuckDan');
@@ -50,8 +52,15 @@ app.get('/api/doctor/find', function(req, res) {
   dbHelpers.getDocs(targetDocs, res);
 });
 
-app.post('/api/symptomEntry/add', function(req, res) {
-  var newSympson = req.body;
-  console.log(newSympson);
-  dbHelpers.addSymptom(newSympson, res);
+// USER SIGNUP SIGNIN
+
+app.post('/api/signup', function(req, res, next) {
+  var userSignup = req.body;
+  dbHelpers.signup(userSignup, res, next);
+});
+
+app.post('/api/signin', function(req, res, next) {
+  var userSignin = req.body;
+  console.log('usersignin server', userSignin);
+  dbHelpers.signin(userSignin, res, next);
 });
