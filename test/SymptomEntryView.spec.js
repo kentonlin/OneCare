e70c33bbbbd5a759d2aa1a942b6396817f7f105c
danielzhao <danielzhaozd@gmail.com>
2016-08-04 16:25:12 -0400

    wrote passing tests

--- test/SymptomEntryView.spec.js
+++ test/SymptomEntryView.spec.js
@@ -1,9 +1,9 @@
 import React from 'react'; 
 import { shallow } from 'enzyme'; // Method from enzyme which allows us to do shallow render
-import App from '../client/app/app.jsx';
+import SymptomEntryView from '../client/app/symptomEntry.jsx';
 
-describe('App', () => {
-  const wrapper = shallow(<App />);
+describe('SymptomEntryView', () => {
+  const wrapper = shallow(<SymptomEntryView />);
 
   it('works!', () => {
     expect(true).to.be.true;
