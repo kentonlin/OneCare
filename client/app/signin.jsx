29719e6c9c646fd52e4e071091af0d4cbbf81155
Kenton <kentonlin11@gmail.com>
2016-08-04 15:49:00 -0400

    rebase

--- client/app/signin.jsx
+++ client/app/signin.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Router, Route, Link, browserHistory } from 'react-router';
 import $ from 'jquery';
+import ReactDOM from 'react-dom';
+import Signup from './signup.jsx'
 
 export default class Signin extends React.Component {
   constructor(props) {
@@ -10,7 +12,13 @@ export default class Signin extends React.Component {
       password: ""
     };
     this.submitLogin = this.submitLogin.bind(this);
+    this.signup = this.signup.bind(this);
   }
+
+  componentDidMount() {
+    window.localStorage.currentPage = 'signin';
+  }
+
 
   submitLogin(e) {
     e.preventDefault();
@@ -42,6 +50,12 @@ export default class Signin extends React.Component {
     });
   }
 
+  signup() {
+    console.log("wtf", window.localStorage);
+    window.localStorage.currentPage = "signup";
+    ReactDOM.render(<div> <Signup/> </div>, document.getElementById("app"));
+  }
+
   render() {
     return (
       <div className="signin-container">
@@ -50,7 +64,7 @@ export default class Signin extends React.Component {
         <form>
           <span>username</span><input type="text" onChange={(event) => {this.setState({username: event.target.value})}}></input><br />
           <span>password</span><input type="password" onChange={(event) => {this.setState({password: event.target.value})}}></input><br />
-          <h2> Signup create a new account </h2>
+          <button onClick= {this.signup}> Signup create a new account </button>
           <button onClick={ this.submitLogin}>Submit</button>
         </form>
       </div>
