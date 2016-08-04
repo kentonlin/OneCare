9345ad61655962fcc90b63047e91d02c8d6570e8
danielzhao <danielzhaozd@gmail.com>
2016-08-04 15:56:06 -0400

    aeu

--- client/app/app.jsx
+++ client/app/app.jsx
@@ -30,6 +30,5 @@ class App extends React.Component {
       )
   }
 }
-if(typeof window !== 'undefined') {
-    React.render(<div><App /></div>, document.getElementById("app"));
-}
+
+ReactDOM.render(<div><App /></div>, document.getElementById("app"));
