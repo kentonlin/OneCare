bcd8df30d85f886706fbd0f38308834e480f34d5
Harish Kilaru <hckilaru@gmail.com>
2016-08-09 17:16:24 -0400

    deleted bundle

--- client/app/signup.jsx
+++ client/app/signup.jsx
@@ -11,7 +11,9 @@ export default class Signup extends React.Component {
       address: "",
       phone: "",
       zipcode: "",
-      email: ""
+      email: "",
+      invalidPhone: false,
+      invalidEmail: false
     };
     this.submitUser = this.submitUser.bind(this);
     this.validatePhone = this.validatePhone.bind(this);
@@ -52,12 +54,18 @@ export default class Signup extends React.Component {
   }
 
   validatePhone(phone) {
-    return phone.match(/\d/g).length===11;
+    this.setState({
+      invalidPhone: phone.match(/\d/g).length===10
+    })
   }
 
   validateEmail(email) {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+    console.log("test", re.test(email));
+    console.log("state", this.state.invalidEmail)
+    this.setState({
+      invalidEmail: re.test(email)
+    })
   }
 
   render() {
@@ -69,8 +77,10 @@ export default class Signup extends React.Component {
           <span>password</span><input type="password" onChange={(event) => {this.setState({password: event.target.value})}}/><br />
           <span>address</span><input type="text" onChange={(event) => {this.setState({address: event.target.value})}}/><br/>
           <span>zip code</span><input type="text" onChange={(event) => {this.setState({zipCode: event.target.value})}}/><br/>
-          <span>phone</span><input type="text" validation={this.validatePhone} onChange={(event) => {this.setState({phone: event.target.value})}}/><br/>
-          <span>email</span><input type="text" validation={this.validateEmail} onChange={(event) => {this.setState({email: event.target.value})}}/><br/>
+          <div>phone</div><input type="text" onChange={(event) => {this.setState({phone: event.target.value})
+        this.validatePhone(this.state.phone)}}/> <h6 className={(this.state.invalidPhone ? 'hidden' : '')}> Phone number must be 11 digits</h6>
+          <div>email</div><input type="text" onChange={(event) => {this.setState({email: event.target.value})
+          this.validateEmail(this.state.email)}}/><h6 className={(this.state.invalidEmail ? 'hidden' : '')}> Enter a valid email</h6>
           <button onClick={ this.submitUser }>Submit</button>
         </form>
         <Link to="/signin">Signin </Link>
