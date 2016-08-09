0e7075b8801566bc6952fc7e87fc35bf3e849177
Harish Kilaru <hckilaru@gmail.com>
2016-08-09 17:15:41 -0400

    conflict

--- client/app/signup.jsx
+++ client/app/signup.jsx
@@ -10,9 +10,12 @@ export default class Signup extends React.Component {
       password: "",
       address: "",
       phone: "",
-      zipcode: ""
+      zipcode: "",
+      email: ""
     };
     this.submitUser = this.submitUser.bind(this);
+    this.validatePhone = this.validatePhone.bind(this);
+    this.validateEmail = this.validateEmail.bind(this);
   }
 
   submitUser(e) {
@@ -24,7 +27,8 @@ export default class Signup extends React.Component {
       password: this.state.password,
       address: this.state.address,
       phone: this.state.phone,
-      zipcode: this.state.zipcode
+      zipcode: this.state.zipcode,
+      email: this.state.email
     };
 
     $.ajax({
@@ -47,21 +51,26 @@ export default class Signup extends React.Component {
     });
   }
 
-  // validateNumber(e) {
-  //   e.preventDefault();
-  //
-  // }
+  validatePhone(phone) {
+    return phone.match(/\d/g).length===11;
+  }
+
+  validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
 
   render() {
     return (
       <div className= "signup-container">
         <h1> Signup </h1>
         <form>
-          <span>username</span><input type="text" onChange={(event) => {this.setState({username: event.target.value})}}></input><br />
-          <span>password</span><input type="password" onChange={(event) => {this.setState({password: event.target.value})}}></input><br />
-          <span>address</span><input type="text" onChange={(event) => {this.setState({address: event.target.value})}}></input><br />
-          <span>zip code</span><input type="text" onChange={(event) => {this.setState({zipCode: event.target.value})}}></input><br />
-          <span>phone</span><input type="text" onChange={(event) => {this.setState({phone: event.target.value})}}></input><br />
+          <span>username</span><input type="text" onChange={(event) => {this.setState({username: event.target.value})}}/><br/>
+          <span>password</span><input type="password" onChange={(event) => {this.setState({password: event.target.value})}}/><br />
+          <span>address</span><input type="text" onChange={(event) => {this.setState({address: event.target.value})}}/><br/>
+          <span>zip code</span><input type="text" onChange={(event) => {this.setState({zipCode: event.target.value})}}/><br/>
+          <span>phone</span><input type="text" validation={this.validatePhone} onChange={(event) => {this.setState({phone: event.target.value})}}/><br/>
+          <span>email</span><input type="text" validation={this.validateEmail} onChange={(event) => {this.setState({email: event.target.value})}}/><br/>
           <button onClick={ this.submitUser }>Submit</button>
         </form>
         <Link to="/signin">Signin </Link>
