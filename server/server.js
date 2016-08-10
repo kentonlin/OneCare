f3d0d8962b3d29602099e611728e50c0f05ee1f1
danielzhao <danielzhaozd@gmail.com>
2016-08-10 10:18:42 -0400

    marge

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ var path = require('path');
 var app = express();
 var brain = require('./brain.js');
 var twilio = require('twilio');
+var Yelp = require('yelp');
+
 
 app.use(express.static('public'));
 
@@ -40,8 +42,8 @@ app.post('/api/user/zip', function(req, res) {
 app.post('/api/reminder/add', function(req, res, next) {
 
   console.log('request received at addScriptReminder');
-	var newScript = req.body;
-	dbHelpers.addScript(newScript, res, next);
+  var newScript = req.body;
+  dbHelpers.addScript(newScript, res, next);
 });
 
 app.post('/api/reminder/delete', function(req, res, next){
@@ -51,8 +53,8 @@ app.post('/api/reminder/delete', function(req, res, next){
 });
 
 app.post('/api/script/find', function(req, res) {
-	var username = req.body.username;
-	dbHelpers.getScripts( username, res);
+  var username = req.body.username;
+  dbHelpers.getScripts( username, res);
 });
 
 app.post('/api/doctor/add', function(req, res) {
@@ -132,11 +134,30 @@ app.post('/api/reminder/delete', function(req, res, next){
 
 app.post('/api/brain/add', function(req, res) {
   var pair = req.body.pair;
-  res.send(brain.OCBrain.addTrainingPair(pair))
+  res.send(brain.OCBrain.addTrainingPair(pair));
   brain.OCBrain.train(1);
   brain.OCBrain.save("MainBrain");
 });
 
+app.get('/yelpshit', function(req, res) {
+  yelp.search({ term: 'doctor', location: 'New+York' })
+  .then(function(data) {
+    console.log(data);
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
+});
+
+var yelp = new Yelp({
+  consumer_key: 'KdzFVkM8FBBCGrbDDyxtwg',
+  consumer_secret: 'I8yoCqbXPUq6gm76SwManEl19Oo',
+  token: 'OUS1wKk36mJckzoMqbeBz7tKE16K3oSK',
+  token_secret: 'WYZ_atOGdkzRy3OnsegGf0JkonU'
+});
+
+
 app.get('/*', function(req, res) {
   res.sendFile(path.join(rootPath + "/index.html"));
 });
+
