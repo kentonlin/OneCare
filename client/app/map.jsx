1630f3767994ee7723d1f6c8ec5c96a79a61a856
Kenton <kentonlin11@gmail.com>
2016-08-11 16:54:28 -0400

    rebase 4:54

--- client/app/map.jsx
+++ client/app/map.jsx
@@ -6,20 +6,21 @@ export default class Map extends React.Component {
     super(props);
     this.state = {
       modalIsOpen: true,
-      inputzipcode: 0
     };
   }
+
+
 
   render() {
     return (
       // var srcURL = "https://www.google.com/maps/embed/v1/search?key=AIzaSyCnPK2o-dXX9hTQdMA4dTXIezhxyIzfRB0&q=pharmacy+near+" + {this.state.zipcode}
-      // if inputzipcode state is 0 then use the users zipcode, otherwise use the inputzipcode 
+      // if inputzipcode state is 0 then use the users zipcode, otherwise use the inputzipcode
       <iframe
       width="650"
       height="450"
       // frameborder="0"
       // style="border:0"
-      src="https://www.google.com/maps/embed/v1/search?key=AIzaSyCnPK2o-dXX9hTQdMA4dTXIezhxyIzfRB0&q=pharmacy+near+10001" allowfullscreen>
+      src={"https://www.google.com/maps/embed/v1/search?key=AIzaSyCnPK2o-dXX9hTQdMA4dTXIezhxyIzfRB0&q=pharmacy+near+"+this.props. zipcode}>
       </iframe>
     );
   }
