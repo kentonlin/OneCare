6eb47be8e86d78f58464ac1ece18042d43c76b9d
danielzhao <danielzhaozd@gmail.com>
2016-08-15 19:04:12 -0400

    merge33

--- client/app/navigate.jsx
+++ client/app/navigate.jsx
@@ -33,11 +33,8 @@ componentDidMount(){
         <div className="top"> 
           <img className='logo' src='https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/12316641_10105768042772978_923906279489141499_n.jpg?oh=bbb5b2be0e9bba21d5b33d879a199c4e&oe=58574F1F'></img>
         </div>
-        <div className="navbar-button" onClick={() => {window.localStorage.removeItem("username"); window.localStorage.removeItem("token");}}>
+        <div onClick={() => {window.localStorage.removeItem("username"); window.localStorage.removeItem("token");}}>
           <Link to='/signin'><Button bsSize='large' bsStyle='danger' >Logout</Button></Link>
-        </div>
-        <div className="navbar-button">
-          <Link to='/recommend'> Physician Recommender </Link>
         </div>
       </div>
     );
