3bd779c10a2c170efdafe49466b6b9e2d0e5efce
Kenton <kentonlin11@gmail.com>
2016-08-20 17:01:11 -0400

    update

--- client/app/splash.jsx
+++ client/app/splash.jsx
@@ -10,52 +10,26 @@ import {Image} from 'react-bootstrap';
 export default class Splash extends React.Component {
   constructor(props) {
     super(props);
-    // this.state= {
-    //   openSignup: false,
-    //   openLogin: false
-    // };
-    // this.openSignupPage = this.openSignupPage.bind(this);
-    // this.openLoginPage = this.openLoginPage.bind(this);
-  }
 
-  // openSignupPage() {
-  //   this.setState({
-  //     openSignup: true
-  //   });
-  // }
-  //
-  // openLoginPage() {
-  //   this.setState({
-  //     openLoginPage: true
-  //   })
-  // }
+    render() {
+      return (
+        <div>
+          <div className= "splashHeader">
+          {/* <video autoPlay loop id="video-background">
+            <source id="mp4" src="./mountain.mp4" type="video/mp4"/>
+          </video> */}
+            <div className= "splashName">
+              <h2> OneCare </h2>
+            </div>
 
+            <div className= "signInSplash">
+              <Link to='/signin'> Login </Link>
+            </div>
 
-  render() {
-    return (
-      <div>
-
-
-
-        <div className= "splashHeader">
-
-          <video autoPlay loop id="video-background">
-            <source id="mp4" src="https://fpdl.vimeocdn.com/vimeo-prod-skyfire-std-us/01/4528/6/172644004/557095694.mp4?token=57b5142f_0x33fefb0a9c04f360f3eb118144c0fdfe4cd333dd" type="video/mp4"/>
-          </video>
-
-          <div className= "splashName">
-            <h2> OneCare </h2>
+            <div className= "signUpSplash">
+              <Link to='/signup'> Signup </Link>
+            </div>
           </div>
-
-          <div className= "signInSplash">
-            <Link to='/signin'> Login </Link>
-          </div>
-
-          <div className= "signUpSplash">
-            <Link to='/signup'> Signup </Link>
-          </div>
-        </div>
-
 
         <div className= "appDescribe">
           <div> Prescription Reminder</div>
@@ -67,18 +41,6 @@ export default class Splash extends React.Component {
           <h3> Recommend a specialist </h3>
           <h3> Search for nearby specialists </h3>
         </div>
-
-
-
-
-
-
-
-
-
-
-
-
 
               {/* <ButtonGroup vertical>
         <Button>Button</Button>
@@ -98,8 +60,6 @@ export default class Splash extends React.Component {
           <MenuItem eventKey="2">Dropdown link</MenuItem>
         </DropdownButton>
       </ButtonGroup> */}
-
-
       </div>
     );
   }
