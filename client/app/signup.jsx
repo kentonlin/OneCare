c15fddddb2a43c8ebaf438763f47a2d15bdd37c5
Kenton <kentonlin11@gmail.com>
2016-08-20 11:05:30 -0400

    changes to signup

--- client/app/signup.jsx
+++ client/app/signup.jsx
@@ -184,8 +184,13 @@ export default class Signup extends React.Component {
           {/* </Col> */}
           {/* <Col md={4}> */}
           <FormGroup>
-            <ControlLabel>Zipcode</ControlLabel><FormControl type="text" onChange={this.handleZipcode}></FormControl></FormGroup>
-            <h6 className={(this.state.zipcodeIsValid ? 'hidden' : 'invalid')}> Please enter a valid zipcode </h6>
+            {/* <div className='signup-input'> */}
+              <ControlLabel>Zipcode</ControlLabel><FormControl type="text" onChange={this.handleZipcode}></FormControl>
+            {/* </div> */}
+            {/* <div className="signup-error"> */}
+              <h6 className={(this.state.zipcodeIsValid ? 'hidden' : 'invalid')}> Please enter a valid zipcode </h6>
+            {/* </div> */}
+          </FormGroup>
           {/* </Col> */}
           {/* <Col mdOffset={2} md={4}> */}
           <FormGroup  >
