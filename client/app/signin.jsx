2c95a2c79e3eea1872461994efe06bc686472a2b
Harish Kilaru <hckilaru@gmail.com>
2016-08-23 09:59:28 -0400

    mc

--- client/app/signin.jsx
+++ client/app/signin.jsx
@@ -54,13 +54,13 @@ export default class Signin extends React.Component {
     return (
       <div className="signin-container">
         <Grid>
-          <Row className='signin-text'>
-            <h1>Sign in to OneCare</h1>
+          <Row>
+            <h1 className='white'>Sign in to OneCare</h1>
           </Row>
           <Row>
             <Form>
-              <div className='signin-cat'> <span className='signin-text' >Username</span>&nbsp;&nbsp;<input type="text" onChange={(event) => {this.setState({username: event.target.value})}}></input></div><br />
-              <div className='signin-cat2'> <span className='signin-text' >Password</span>&nbsp;&nbsp;<input type="password" onChange={(event) => {this.setState({password: event.target.value})}}></input></div><br />
+              <div className='signin-cat'> <span className='signin-username white' >Username</span>&nbsp;&nbsp;<input type="text" onChange={(event) => {this.setState({username: event.target.value})}}></input></div><br />
+              <div className='signin-cat2'> <span className='signin-password white' >Password</span>&nbsp;&nbsp;<input type="password" onChange={(event) => {this.setState({password: event.target.value})}}></input></div><br />
               <Button bsStyle='success' onClick={ this.submitLogin}>Submit</Button>
               <div className='signup'>
               <Link to='/signup'> Signup </Link>
